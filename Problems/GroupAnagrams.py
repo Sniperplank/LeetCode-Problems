24a393d8bb5d89e2bf02dea49d5269cc5cbe25f4
Eilaf Aljundi <97557078+Sniperplank@users.noreply.github.com>
2023-03-09 13:20:31 -0500

    Update GroupAnagrams.py

--- Problems/GroupAnagrams.py
+++ Problems/GroupAnagrams.py
@@ -11,3 +11,18 @@ Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 """
 
 # Problem Solution
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+        initialize a dict with default values of list
+        map all lowercase english letters to a list of 0's
+        for each word creat a count and put a 1 in place of any letter that appears in the word
+        use that count as a key in the dict and append the word to that key's list 
+        '''
+        res = collections.defaultdict(list)
+        for i in strs:
+            count = [0] * 26
+            for x in i:
+                count[ord(x) - ord('a')] += 1
+            res[tuple(count)].append(i)
+        return res.values()
